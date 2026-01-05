124b9a3c4950abcff28dd3f7b8b61dad9d78e540
Łukasz Bączkiewicz <147326943+vienox@users.noreply.github.com>
2026-01-05 21:41:07 +0100

    main logic

--- Program.cs
+++ Program.cs
@@ -1,6 +1,74 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TournamentApi.Auth;
+using TournamentApi.Data;
+using TournamentApi.GraphQL;
+using TournamentApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<JwtOptions>(opt =>
+{
+    opt.Issuer = "TournamentApi";
+    opt.Audience = "TournamentApi";
+    opt.Key = "secretkey123";
+    opt.ExpMinutes = 120;
+});
+
+builder.Services.AddSingleton<JwtTokenService>();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite("Data Source=tournament.db");
+});
+
+builder.Services.AddScoped<BracketService>();
+
+var jwtOpt = new JwtOptions();
+builder.Configuration.Bind("Jwt", jwtOpt); 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var key = builder.Services.BuildServiceProvider()
+            .GetRequiredService<Microsoft.Extensions.Options.IOptions<JwtOptions>>().Value;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = key.Issuer,
+            ValidAudience = key.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key.Key)),
+            NameClaimType = "sub"
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services
+    .AddGraphQLServer()
+    .AddAuthorization()
+    .AddQueryType<Query>()
+    .AddMutationType<Mutation>()
+    .AddProjections()
+    .AddFiltering()
+    .AddSorting();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// DB init
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGraphQL("/graphql");
 app.Run();
